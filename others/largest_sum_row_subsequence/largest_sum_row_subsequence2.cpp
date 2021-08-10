5d365b9164b9f86bb9b0189f4ccb93074f346c0c
MikhailPimenov <misha4git@yandex.ru>
2021-08-10 20:08:51 +0300

    largest_row_sum_subsequence2 - refactored: removed else-statements and replaced 'do-while' with just 'while'

--- others/largest_sum_row_subsequence/largest_sum_row_subsequence2.cpp
+++ others/largest_sum_row_subsequence/largest_sum_row_subsequence2.cpp
@@ -5,13 +5,13 @@ static void update_maximum(int current_sum, int& max_sum) {
 		max_sum = current_sum;
 }
 static void move_right(const int* a, int length, int& right, int& current_sum) {
-	assert(right + 1 < length && "Right index is out of range!");
+	assert(right < length && "Right index is out of range!");
 
 	++right;
 	current_sum += a[right];
 }
 static void move_left(const int* a, int length, int& left, int& current_sum) {
-	assert(left + 1 < length && "Left index is out of range!");
+	assert(left < length && "Left index is out of range!");
 
 	current_sum -= a[left];
 	++left;
@@ -28,20 +28,22 @@ int largest_sum_row_subsequence2(const int* a, int length) {
 	int left{ 0 };
 	int right{ 0 };
 
-	do{
-		if (current_sum >= 0 && right + 1 < length) {
-			move_right(a, length, right, current_sum);
-		}
-		else if (right == left) {
-			move_both(a, length, left, right, current_sum);
-		}
-		else { // (current_sum < 0 or right + 1 == length) and right != left
-			move_left(a, length, left, current_sum);
-		}
-
+	while (right < length && left < length) {
 		update_maximum(current_sum, max_sum);
 
-	} while (right + 1 < length && left + 1 < length);
+		if (current_sum >= 0 && right + 1 < length) {
+			move_right(a, length, right, current_sum);
+			continue;
+		}
+	
+		if (right == left) {
+			move_both(a, length, left, right, current_sum);
+			continue;
+		}
+		
+		// (current_sum < 0 or right + 1 == length) and right != left
+		move_left(a, length, left, current_sum);
+	}
 
 	return max_sum;
 }
